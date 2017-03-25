335b5f454b3dcf4a1af2f447730285192e090d95
vogdev <vogdev@gmail.com>
2017-03-25 13:44:13 +0000

    i added the CRUD Actions for Posts & i added devise for authentication

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,52 @@
+class PostsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :set_post_owner, only: [:edit, :update, :destroy]
+	def index
+		@posts = Post.all
+	end
+	def show  
+	end 
+	def new
+		@post = current_user.posts.build
+	end
+	def create  
+	 @post = current_user.posts.build(post_params)
+		if @post.save
+			flash[:success] = "Post created"
+			redirect_to posts_path
+			else
+			flash.now[:alert] = "something went wrong."
+			render :new
+		end
+	end  
+	def edit
+	end
+	def update
+		if @post.update(post_params)
+			flash[:success] = "Post updated"
+			redirect_to(post_path(@post))
+		else
+			flash.now[:alert] = "something went wrong."
+			render :edit
+		end
+	end
+	def destroy
+		@post.destroy
+		redirect_to posts_path
+	end
+	private
+	def post_params
+	params.require(:post).permit(:caption, :image)
+	end
+	def set_post
+		@post = Post.find(params[:id])
+	end
+	def set_post_owner  
+    unless current_user == @post.user
+     flash[:alert] = "NOOOOOP!!!!"
+     redirect_to root_path
+    end
+  end  
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+	belongs_to :user
+	validates :image, presence: true
+	has_attached_file :image, styles: { original: "600x600>", thumb: "300x300>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+end
